3a0f1869d29f466f8bd7f85f7cc97f4cceb001e8
hoanggiakz <126653307+hoanggiakz@users.noreply.github.com>
2023-11-14 17:48:30 +0700

    Add files via upload

---
+++ TimMangCon.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class TimMangCon {
+    public static void main(String[] args) {
+        int[] mang = {1, 2, 3, 7, 5};
+        int S = 12;
+
+        ArrayList<Integer> ketQua = timMangCon(mang, S);
+
+        System.out.println("Vi Tri: " + ketQua.get(0) + " " + ketQua.get(1));
+    }
+
+    private static ArrayList<Integer> timMangCon(int[] mang, int S) {
+        ArrayList<Integer> ketQua = new ArrayList<>();
+
+        int tong = 0;
+        int batDau = 0;
+
+        for (int i = 0; i < mang.length; i++) {
+            tong += mang[i];
+
+            while (tong > S) {
+                tong -= mang[batDau];
+                batDau++;
+            }
+
+            if (tong == S) {
+                // Tìm thấy mảng con có tổng bằng S
+                ketQua.add(batDau + 1); // Chỉ số bắt đầu (1-based indexing)
+                ketQua.add(i + 1);       // Chỉ số kết thúc (1-based indexing)
+                return ketQua;
+            }
+        }
+
+        
+        ketQua.add(-1);
+        return ketQua;
+    }
+}
